52d8f9f4ad3dea4d46a4c0d2fb1ee5481e1abb0a
Wissem Sahli <contact.ing.wissem@gmail.com>
2026-07-18 18:38:42 +0100

    edits

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
 
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestClassifiers
+# pyrefly: ignore [missing-import]
+from xgboost import XGBClassifier
+
+# DATA 
+
 data = []
 labels = []
 
@@ -24,3 +32,15 @@ data = np.asarray(data)
 labels = np.asarray(labels)
 
 print(len(data),len(labels))
+
+# Train / Test split
+
+x_train , x_train , y_train , y_test = train_test_split( data , labels , 
+                                                test_size = 0.2, 
+                                                shuffle = True,
+                                                stratify= labels,
+                                                random_state=42)
+
+
+
+
